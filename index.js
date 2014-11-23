6bda762c8eea8fdf80e3232d91bd979837651c02
nishidy <nishidyatutokyo@gmail.com>
2014-11-23 14:56:13 +0900

    Use AWS from heroku

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-var express = require('express')
+var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+var AWS = require('aws-sdk');
+AWS.config.region = 'Tokyo';
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+var s3 = new AWS.S3();
+s3.getObject(
+	{ Bucket:'nishidy-heroku', Key:'test' },
+	function(err,data) {
+		app.get(
+			'/',
+			function(request, response) {
+				response.send(data);
+			}
+		);
+	}
+);
+
+app.listen(
+	app.get('port'),
+	function() {
+		console.log("Node app is running at localhost:" + app.get('port'))
+	}
+)
+
