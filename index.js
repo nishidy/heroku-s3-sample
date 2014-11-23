468176c8b2dec08643b7017208d115af700b2f5d
nishidy <nishidyatutokyo@gmail.com>
2014-11-23 15:23:48 +0900

    Fixed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 var AWS = require('aws-sdk');
-AWS.config.region = 'Tokyo';
+AWS.config.region = 'ap-northeast-1';
 
 var s3 = new AWS.S3();
 s3.getObject(
@@ -14,7 +14,12 @@ s3.getObject(
 		app.get(
 			'/',
 			function(request, response) {
-				response.send(data);
+				if(data==null){
+					response.send("No data...");
+					console.log(err);
+				}else{
+					response.send("<html><head><title>test</title></head><body>"+String(data.Body)+"</body></html>");
+				}
 			}
 		);
 	}
